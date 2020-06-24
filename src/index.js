b949b5a876de8e5761436526de5a913efc2fa8e6
Bruno Staub <bruno.staub@gmail.com>
2020-06-24 20:29:30 +0200

    color game part 1

--- src/index.js
+++ src/index.js
@@ -1,10 +1,100 @@
 import './styles.scss';
 
-const square = document.querySelectorAll('.square');
-const squareArray = Array.from(square);
-console.log(squareArray);
+const domQueryStrings = {
+  square: document.querySelectorAll('.square'),
+  displayRandomColor: document.querySelector('.displayRandomColor'),
+  displayResult: document.querySelector('.bar span'),
+  h1Bar: document.querySelector('h1'),
+  buttonNewColor: document.querySelector('button.newColors'),
+};
 
-const colors = ['rgb(255, 0, 0)', 'rgb(255, 0, 0)'];
+/*
+const colors = [
+  'rgb(255, 0, 0)',
+  'rgb(255, 255, 0)',
+  'rgb(255, 0, 255)',
+  'rgb(0, 255, 0)',
+  'rgb(0, 0, 255)',
+  'rgb(255, 0, 255)',
+];
+*/
+
+/*
+class Colorrandom {
+  constructor(colorsarray, randomcolor) {
+    this.colorsarray = colorsarray;
+    this.randomcolor = randomcolor;
+  }
+
+
+}
+*/
+
+// Function Block
+function generateColors(anzahl) {
+  const tempArray = [];
+  for (let i = 0; i < anzahl; i++) {
+    const r = Math.floor(Math.random() * 256);
+    const g = Math.floor(Math.random() * 256);
+    const b = Math.floor(Math.random() * 256);
+    tempArray.push(`rgb(${r}, ${g}, ${b})`);
+  }
+  return tempArray;
+}
+let colors = generateColors(5);
+
+
+
+function pickColor() {
+  const random = Math.floor(Math.random() * colors.length);
+  return colors[random];
+}
+let guessColor = pickColor();
+
+
+
+
+
+
+
+
+
+
+domQueryStrings.buttonNewColor.addEventListener('click', () => {
+  // generate new Colors
+  colors = generateColors(5);
+  // change Display Color to match picket Color!!!!
+  guessColor = pickColor();
+  // show new guessColor
+  domQueryStrings.displayRandomColor.textContent = guessColor;
+  // change color of squares (reset new colors)
+  for (let i = 0; i < domQueryStrings.square.length; i++) {
+    domQueryStrings.square[i].style.backgroundColor = colors[i];
+    domQueryStrings.h1Bar.style.backgroundColor = 'black';
+  }
+});
+
+
+
+domQueryStrings.square.forEach((item, index) => {
+  item.style.backgroundColor = colors[index];
+
+  item.addEventListener('click', function () {
+    const clickedColor = this.style.backgroundColor;
+
+    if (guessColor === clickedColor) {
+      domQueryStrings.displayResult.textContent = 'Right Color';
+      for (let i = 0; i < domQueryStrings.square.length; i++) {
+        domQueryStrings.square[i].style.backgroundColor = guessColor;
+        domQueryStrings.h1Bar.style.backgroundColor = guessColor;
+      }
+    } else {
+      domQueryStrings.displayResult.textContent = 'Wrong Color';
+      // item.style.backgroundColor = 'black';
+      this.style.backgroundColor = 'black';
+    }
+  });
+});
 
 
 
